8cd91977081a2bab997d2f252d20c350b60d6581
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-24 11:40:40 -0300

    Add Function de Event para add e Deletar anotação

--- js/script.js
+++ js/script.js
@@ -47,3 +47,30 @@ function addHTML(item) {
 
     content.appendChild(div);
 }
+
+function addEvents() {
+    const notes = document.querySelectorAll(".item textarea");
+    const remove = document.querySelectorAll(".item .remove");
+
+    notes.forEach((item, i) => {
+        item.oninput = () => {
+            items_db[i] = {
+                text: item.value,
+                color: items_db[i]?.color || item.parentElement.style.backgroundColor,
+            };
+
+            localStorage.setItem("items_db", JSON.stringify(items_db));
+        };
+    });
+
+    remove.forEach((item, i) => {
+        item.onclick = () => {
+            content.children[i].remove();
+            if (items_db[i]) {
+                items_db.splice(i, 1);
+                localStorage.setItem("items_db", JSON.stringify(items_db));
+            }
+            addEvents();
+        };
+    });
+}
